f9a06d2e0b9d8a7f9a72461187830e91b60f3f0f
rgmcwill <rgmcwill@uark.edu>
2019-01-31 15:21:38 -0600

    fixed problems with database formating and changed some setting

--- softball/settings.py
+++ softball/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['forkb-sds.azurewebsites.net','localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
