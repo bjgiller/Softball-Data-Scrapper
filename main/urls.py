03d29ca9b9e689b0c3c1603fa958a53ed83a0f36
Flora Smith <ftsmith@email.uark.edu>
2019-02-20 17:02:27 -0600

    fixed urls.py to load CurrentRanking.html first

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('',views.index,name='index'),
+	path('',views.CurrentRanking,name='CurrentRanking'),
 	path('index',views.index,name='index'),
 	path('CurrentRanking',views.CurrentRanking,name='CurrentRanking')
 ]
